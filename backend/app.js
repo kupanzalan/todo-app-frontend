05b9f63c0f37d9467df6cd3be259d19130227733
Kupán Zalán <zalan_kupan@yahoo.com>
2023-10-10 11:10:46 +0300

    Removed unused comments

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const cors = require('cors');
-const taskRouter = require('./router'); // Update the path if needed
+const taskRouter = require('./router');
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Replace with the actual origin of your React Native app
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
 app.use('/api', taskRouter); 
-
-// Define your routes and middleware here.
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
